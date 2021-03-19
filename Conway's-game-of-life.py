dbffff2f2e0276785c3520e3a93eac2f2f13c088
thomasdelatte <thomasdelatte@hotmail.com>
2021-03-19 15:31:14 +0100

    Deleted copy of game-of-life script.

--- Conway's-game-of-life.py
+++
@@ -1,3 +0,0 @@
-# Short program: Conway's Game of Life
-# It is an example of cellular automata: a set of rules governing the behavior of a field made up of discrete cells.
-# Even though the rules re simple, some surprising behaviors emerge: the patterns can move, self-replicate and even mimic CPUs.
