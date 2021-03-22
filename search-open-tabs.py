be88fa404d14dea71a81368d9ce42bb4861a7c25
thomasdelatte <thomasdelatte@hotmail.com>
2021-03-22 11:47:55 +0100

    Added html parser with bs4.

--- search-open-tabs.py
+++ search-open-tabs.py
@@ -1,10 +1,11 @@
 #! python3
+# search-open-tabs.py
 # A small script to open the top search results from Google in tabs
 
 import bs4,requests, sys, webbrowser
 
 # Display text while searching for results
-print("Searchig...") 
+print("Searching...") 
 
 # Get the web page of the Google search with command line arguments
 res = requests.get("https://google.be/search?q=" + " ".join(sys.argv[1:]))
@@ -12,5 +13,14 @@ res = requests.get("https://google.be/search?q=" + " ".join(sys.argv[1:]))
 # Check status of HTTP request
 res.raise_for_status()
 
-# TODO: Retrieve top search result links.
-# TODO: Open a browser tab for each result
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(res.text, "html.parser")
+
+# Open a browser tab for each result    
+for g in soup.find_all("div", class_= "yuRUbf"):
+    anchors = g.find_all("a")
+    for i in range(0, 5):
+        linkToOpen = anchors[i]["href"]
+        title = g.find("h3").text
+        print("Opening" + title)
+        webbrowser.open(linkToOpen)
